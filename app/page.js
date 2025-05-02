d46081cb20a3d98406c73e1f03b9d55a7f1471f5
Vinicius Rivaldar dos Santos Silva <149613214+ViniRivaldar@users.noreply.github.com>
2025-05-02 19:36:37 +0000

    teminado o home

--- app/page.js
+++ app/page.js
@@ -1,7 +1,15 @@
+import CardHome from "./components/CardHome";
+
 export default function Home() {
   return (
-    <div className="w-[1192px] bg-[#191970] mt-10">
-      <h1>ola</h1>
+    <div className="w-[1192px] bg-[#191970] mt-10 mb-10 p-10">
+      <CardHome/>
+      <CardHome/>
+      <CardHome/>
+      <CardHome/>
+      <CardHome/>
+      <CardHome/>
+      <CardHome/>
     </div>
   );
 }
